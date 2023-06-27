edabd9b380c1bb8cc8ec8d237efbe2bb9cd91550
DEVIKA S KRISHNAN <devikaskrishnan2018@gmail.com>
2023-06-27 11:07:14 +0530

    pedning

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home, :index]
+
   skip_before_action :authenticate_user!, only: [:root]
   def home
   end
